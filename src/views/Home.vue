07998f87ae1557ab7b8766c643717322a9d0fd32
andy <zhangandi@360.cn>
2021-11-18 09:02:46 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{info.name}}
+    {{info.age}}
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { ref, defineComponent } from '@vue/composition-api'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
+export default defineComponent({
+  setup() {
+    const info = ref({
+      name: 'andy',
+      age: 28
+    })
+    return {
+      info
+    }
+  }
 })
-export default class Home extends Vue {}
+
 </script>
